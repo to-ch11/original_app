916542669d2f6c9f898a4a69e7552b85deb817bf
EC2 Default User <ec2-user@ip-172-31-17-1.ap-northeast-1.compute.internal>
2022-10-24 20:48:32 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
+  root to: "home#index"
+end
---
+++ db/migrate/20221023003209_create_factories.rb
@@ -0,0 +1,9 @@
+class CreateFactories < ActiveRecord::Migration[7.0]
+  def change
+    create_table :factories do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
